bfc49ae95777bc8213ce785917bc04adf86c0f8b
Elaine YUEN <reasonelaine@gmail.com>
2025-07-16 08:16:21 +1000

    new commit from branch

---
+++ markdown_test.R
@@ -0,0 +1,13 @@
+
+  #'"markdown_test"
+  #'author: "Elaine"
+  #'date: "2025-07-08"
+  #'output: 
+  #' html_document: 
+  #'keep_md: true
+  
+#' Here's some prose in a very special comment. Let's summarize the built-in
+
+  #+r, eval = TRUE
+
+print("Hello world")
